f73ecb4e0f57a84b8463a8fc9aaaccedbd4ac1f1
BuczkoBalazs <buci85@gmail.com>
2022-07-26 15:11:29 +0200

    Cocktail and Gallery component done without css.

---
+++ frontend/src/components/Cocktail.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Cocktail({ id, name, howto, ingredients, image }) {
+  return (
+    <div className='cocktail-wrapper' key={id}>
+        <div className='base-info'>
+            <h3>{name}</h3>
+            <img src={image} alt={name} />
+        </div>
+        <div className='extended-info'>
+            <p>{howto}</p>
+            <p>{ingredients}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Cocktail
--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
+import Cocktail from './Cocktail';
 
 function Gallery() {
 
-    const [cocktails, setCocktails] = useState([]);
+    const [cocktails, setCocktails] = useState(null);
 
-    async function getCocktails() {
+    const getCocktails = async () => {
 
         const response = await fetch("http://localhost:3001/cocktails");
         const responseJSON = await response.json();
@@ -13,12 +14,16 @@ function Gallery() {
     }
 
     useEffect(() => {
-        getCocktails()
-      }, []);
+        getCocktails();
+    }, []);
 
     return (
         <Layout>
-            QWERTY
+            <h1>Welcome to our gallery. Feel free to browse</h1>
+            <>
+                {cocktails && cocktails.map(({ id, name, howto, ingredients, image }) => <Cocktail key={id} id={id} name={name} howto={howto} ingredients={ingredients} image={image} />
+                )}
+            </>
         </Layout>
     )
 }
