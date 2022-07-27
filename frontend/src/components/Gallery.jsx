1556fcc37c6364497976685d9089af425cc1202c
BuczkoBalazs <buci85@gmail.com>
2022-07-27 09:58:24 +0200

    Search by name in Gallery + 50% css for Cocktail.

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -5,10 +5,11 @@ import Cocktail from './Cocktail';
 function Gallery() {
 
     const [cocktails, setCocktails] = useState(null);
+    const [searchInput, setSearchInput] = useState("")
 
     const getCocktails = async () => {
 
-        const response = await fetch("http://localhost:3001/cocktails");
+        const response = await fetch('http://localhost:3001/cocktails');
         const responseJSON = await response.json();
         setCocktails(responseJSON);
     }
@@ -19,11 +20,17 @@ function Gallery() {
 
     return (
         <Layout>
-            <h1>Welcome to our gallery. Feel free to browse</h1>
-            <>
-                {cocktails && cocktails.map(({ id, name, howto, ingredients, image }) => <Cocktail key={id} id={id} name={name} howto={howto} ingredients={ingredients} image={image} />
-                )}
-            </>
+            <div className='gallery-wrapper'>
+                <h1>Welcome to our gallery. Feel free to browse</h1>
+                <div className='filters'>
+                    <label for="search">Search by name</label>
+                    <input type="text" name="search" id="search" value={searchInput} onChange={ (e) => setSearchInput(e.target.value) } />
+                </div>
+                <div className='cocktails-wrapper'>
+                    {cocktails && cocktails.map(({ id, name, howto, ingredients, image }) => name.toLowerCase().includes(searchInput.toLowerCase()) && <Cocktail key={id} id={id} name={name} howto={howto} ingredients={ingredients} image={image} />
+                    )}
+                </div>
+            </div>
         </Layout>
     )
 }
