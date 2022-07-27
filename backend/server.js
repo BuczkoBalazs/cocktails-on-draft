89c2e377cf0f2b3c725ff0ef93e0303e162d96a3
BuczkoBalazs <buci85@gmail.com>
2022-07-27 14:31:07 +0200

    feature: Favourites can be added and removed.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path')
 const app = express();
+const fs = require('fs');
 
 app.use(express.json());
 app.use(cors());
@@ -10,6 +11,23 @@ app.use(cors());
 app.get('/cocktails', (req, res) => {
     res.sendFile(path.join(`${__dirname}/data/cocktails.json`))
 });
+
+app.post('/add-fav', (req, res) => {
+    let cocktailsJSON = [];
+    let cocktails = fs.readFileSync(path.join(`${__dirname}/data/cocktails.json`), err => console.log(err));
+
+    cocktailsJSON = JSON.parse(cocktails)
+    let index = cocktailsJSON.map( cocktail => cocktail.id ).indexOf(req.body.id)
+    cocktailsJSON[index].favourite = req.body.favourite.toString()
+
+    fs-fs.writeFileSync(path.join(`${__dirname}/data/cocktails.json`), JSON.stringify(cocktailsJSON, null, 4), err => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+    });
+});
+
 
 app.listen(3001, () =>{
     console.log("Server is running on port 3001")
--- frontend/src/components/Cocktail.jsx
+++ frontend/src/components/Cocktail.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Cocktail({ id, name, howto, ingredients, image, favourite }) {
+
+  const [fav, setFav] = useState(favourite === 'true' ? true : false);
+
+  const favouriteToggle = async () => {
+    await fetch('http://localhost:3001/add-fav', {
+      method: 'POST',
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        id: id,
+        favourite: !fav
+      })
+    })
+    setFav(!fav)
+  };
+
   return (
     <div className='cocktail-wrapper' key={id}>
         <div className='base-info'>
             <h3>{name}</h3>
+            {fav === true ?
+              <button className='fav-button' onClick={favouriteToggle}>Favourite</button> :
+              <button className='fav-button' onClick={favouriteToggle}>Not favourite</button>
+            }
             <img src={image} alt={name} />
         </div>
         <div className='extended-info'>
