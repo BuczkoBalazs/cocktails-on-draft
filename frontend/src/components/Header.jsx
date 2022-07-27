d2e4b922d8949207df6e26f91e1501d38429c1d9
BuczkoBalazs <buci85@gmail.com>
2022-07-27 16:37:08 +0200

    Header content changed.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header className='header'>
-        <p className='logo'>Cocktails on draft</p>
-        <p>Welcome on our site QWERTY!</p>
+        <Link to='/' className='logo'>Cocktails on draft</Link>
         <div className='nav-container'>
           <Link to='/' >Home</Link>
           <Link to='/gallery' >Gallery</Link>
